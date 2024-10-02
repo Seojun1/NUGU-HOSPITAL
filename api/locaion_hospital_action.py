394557d09a8d231b0364f59b94a6881b88587d1f
StandGive <SeoCoding123@gmail.com>
2024-10-03 04:45:27 +0900

    Fixed(location_hospital_action): parameters 구조 변경

--- api/locaion_hospital_action.py
+++ api/locaion_hospital_action.py
@@ -8,11 +8,9 @@ class locationAction(Resource):
         data = request.get_json()
         action = data['action']
         params = action['parameters']
+        location_position = params['location_position']['value']
+        location_city = params['location_city']['value']
 
-        province = params['location_position']['value']
-        city = params['locaion_city']['value']
-        hospital_name = params['hospital_name']['value']
-
-        hospital_location = location_hospital(province, city, hospital_name)
-
-        return create_message_location(f"{hospital_name}의 위치는 {hospital_location} 입니다.")
+        hospital_location = location_hospital(location_position, location_city)
+        print(hospital_location)
+        return create_message_location(f"먼저 {hospital_location[0]}의 위치로는 {hospital_location[1]}이며, 전화번호는 {hospital_location[2]}입니다. {hospital_location[3]}의 위치, {hospital_location[4]} 이고, 전화번호는 {hospital_location[5]}입니다.", location_position, location_city, hospital_location)
