87486609ed7fb98d1aaa2af3ecfbfb8ce5d20941
StandGive <SeoCoding123@gmail.com>
2024-10-03 04:45:50 +0900

    Fixed(all_hospital) : 발화 수정

--- api/all_hospital.py
+++ api/all_hospital.py
@@ -14,7 +14,10 @@ class AllHospitalAction(Resource):
 
         print("백엔드 값 요청 : ", province, city)
         hospital_name = all_hospital(province, city)
-        return create_message(f"{hospital_name} 등등이 있습니다. 더 많은 병원을 알고싶으시다면 '네'라고 말씀해주세요.")
+        if len(hospital_name) == 0:
+            return create_message(f"현재 조회되는 병원이 없습니다. 다음에 저를 다시 불러주세요!")
+        else:
+            return create_message(f"현재 조회되는 병원으로는 총 3곳으로, {hospital_name} 등등이 있습니다. 추가적으로, 더 많은 병원을 목소리로 듣고 싶으시다면 '네'라고 말씀해주세요.", hospital_name, province, city)
 
 class AllHospitalAction2(Resource):
     def post(self):
@@ -27,4 +30,4 @@ class AllHospitalAction2(Resource):
 
         print("백엔드 값 요청 : ", province, city)
         hospital_name = all_hospital_action2(province, city)
-        return create_message_all(f"총 {len(hospital_name)}곳으로, {hospital_name}이 있습니다.")
+        return create_message_all(f"총 {len(hospital_name)}곳으로, {hospital_name}이 있습니다.", hospital_name, province, city)
