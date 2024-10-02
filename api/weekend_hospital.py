244c6fb846971df536374b03e3549775f12ba098
StandGive <SeoCoding123@gmail.com>
2024-10-03 04:46:06 +0900

    Fixed(weekend_hospital): 발화 수정

--- api/weekend_hospital.py
+++ api/weekend_hospital.py
@@ -15,10 +15,12 @@ class WeekendHospitalAction(Resource):
         saturday_hospitals, sunday_hospitals = weekend_hospital(province, city)
         hospitals_cnt = len(saturday_hospitals) + len(sunday_hospitals)
 
+        all_list = saturday_hospitals + sunday_hospitals
+
         if hospitals_cnt == 0:
             return create_weekend_message(f"주말에도 운영하는 병원은 0곳으로, 있지 않은 것 같습니다.")
         else:
-            return create_weekend_message(f"주말에도 운영하는 병원은 {hospitals_cnt}곳으로, 토요일은 {saturday_hospitals}, 일요일은 {sunday_hospitals} 등등이 있습니다.")
+            return create_weekend_message(f"주말에도 운영하는 병원은 {hospitals_cnt}곳으로, 토요일은 {saturday_hospitals}, 일요일은 {sunday_hospitals} 등등이 있습니다.", saturday_hospitals, sunday_hospitals, province, city)
 
 class WeekendAllHospitalAction(Resource):
     def post(self):
@@ -29,5 +31,5 @@ class WeekendAllHospitalAction(Resource):
         city = params['weekend_city']['value']
 
         saturday_hospitals, sunday_hospitals = weekend_all_hospital(province, city)
-        
-        return create_weekend_all_message(f"토요일은 {saturday_hospitals}이 있고, 일요일은 {sunday_hospitals}이 있습니다.")
+
+        return create_weekend_all_message(f"토요일은 {saturday_hospitals}이 있고, 일요일은 {sunday_hospitals}이 있습니다.", saturday_hospitals, sunday_hospitals, province, city)
