bab2c31633cc4de7c5f8da2251c645b919e10292
StandGive <SeoCoding123@gmail.com>
2024-10-03 04:46:19 +0900

    Fixed(holiday_hospital) : 발화 수정

--- api/holiday_hospital.py
+++ api/holiday_hospital.py
@@ -16,7 +16,7 @@ class HolidayHospitalAction(Resource):
         if len(hospital_name) == 0:
             return create_holiday_message(f"공휴일에도 운영하는 병원은 0곳으로, 있지 않은 것 같습니다. 다른 도움이 필요하면 저를 다시 호출해주세요.")
         else:
-            return create_holiday_message(f"공휴일에 운영하는 병원은 {hospital_name} 이 있습니다.")
+            return create_holiday_message(f"공휴일에도 운영하는 병원 3곳을 먼저 알려드릴게요. 공휴일에 운영하는 병원은 {hospital_name} 이 있습니다. 이 외에도 '네'라고 말씀하시면 공휴일에 운영하는 병원 15곳을 더 알려드릴게요.", hospital_name, province, city)
 
 class HolidayHospitalAction2(Resource):
     def post(self):
@@ -29,4 +29,4 @@ class HolidayHospitalAction2(Resource):
 
         hospital_name = holiday_hospital2(province, city)
 
-        return create_holiday2_message(f"{hospital_name} 이 있습니다.")
+        return create_holiday2_message(f"{hospital_name} 이 있습니다.", hospital_name, province, city)
